a01560a6ea4402f7265e11f904e071fd603b3418
LazyTechwork <lazytechwork@gmail.com>
2020-08-09 22:37:03 +0300

    Command processing

---
+++ src/App/Commands/CommandProcessor.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace App\Commands;
+
+
+use DigitalStar\vk_api\LongPoll;
+use DigitalStar\vk_api\vk_api;
+
+class CommandProcessor
+{
+    public const COMMANDS_HOME = "\\App\\Commands\\";
+    private $commands = [];
+    private $namespace = [];
+    private $client;
+    private $lp;
+
+    /**
+     * CommandProcessor constructor.
+     */
+    public function __construct(vk_api $client, LongPoll $lp)
+    {
+        $this->client = $client;
+        $this->lp = $lp;
+    }
+
+
+    /**
+     * Create command
+     *
+     * @param $name
+     * @param $handler
+     * @param boolean $admin
+     */
+    public function createCommand($name, $handler, $admin = false)
+    {
+        $this->commands[$name] = [
+            "handler" => $handler,
+            "admin"   => $admin,
+        ];
+
+        $this->namespace[$name] = $name;
+    }
+
+    /**
+     * Create alias for command
+     *
+     * @param $name
+     * @param $alias
+     */
+    public function createAlias($name, $alias)
+    {
+        $this->namespace[$alias] = $name;
+    }
+
+    /**
+     * Call command by name or alias
+     *
+     * @param $cmd
+     * @param $args
+     * @return mixed
+     */
+    public function callCommand($cmd, $admin, $args)
+    {
+        $command = $this->commands[$this->namespace[$cmd]];
+        if ($command["admin"] && !$admin)
+            return false;
+        [$class, $method] = explode('::', $command["handler"]);
+        return call_user_func([self::COMMANDS_HOME . $class, $method], $this->client, $this->lp, ...$args);
+    }
+}
