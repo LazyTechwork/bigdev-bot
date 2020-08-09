24c72650738c0b666744b8dac10c22b7651a8564
LazyTechwork <lazytechwork@gmail.com>
2020-08-09 20:04:38 +0300

    Setup eloquent

--- public/index.php
+++ public/index.php
@@ -8,12 +8,13 @@ use Dotenv\Dotenv;
 require __DIR__ . '/../vendor/autoload.php';
 
 $config = Dotenv::createImmutable(__DIR__ . '/../');
+$config->load();
 $config->required('BOT_TOKEN')->notEmpty();
+$config->required('DB_DRIVER')->notEmpty();
 $config->required('DB_HOST')->notEmpty();
 $config->required('DB_NAME')->notEmpty();
 $config->required('DB_USER')->notEmpty();
 $config->required('DB_PASS');
-$config->load();
 
 $bot = new Bot();
 $bot->run();
--- src/App/Bot.php
+++ src/App/Bot.php
@@ -2,11 +2,30 @@
 
 namespace App;
 
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 class Bot
 {
+    private $token;
+    private $client;
+    private $capsule;
+
     public function __construct()
     {
-
+        $this->token = $_ENV['BOT_TOKEN'];
+        $this->capsule = new Capsule();
+        $this->capsule->addConnection([
+            'driver'    => $_ENV['DB_DRIVER'],
+            'host'      => $_ENV['DB_HOST'],
+            'database'  => $_ENV['DB_NAME'],
+            'username'  => $_ENV['DB_USER'],
+            'password'  => $_ENV['DB_PASS'],
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]);
+        $this->capsule->setAsGlobal();
     }
 
     public function run()
