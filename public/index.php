6cd3ff2b07d8e65ffcabb4fdb6ccfd002930f9d9
LazyTechwork <lazytechwork@gmail.com>
2020-08-09 19:41:06 +0300

    Added DotEnv

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@
 declare(strict_types=1);
 
 use App\Bot;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$config = Dotenv::createImmutable(__DIR__ . '/../');
+$config->required('BOT_TOKEN')->notEmpty();
+$config->required('DB_HOST')->notEmpty();
+$config->required('DB_NAME')->notEmpty();
+$config->required('DB_USER')->notEmpty();
+$config->required('DB_PASS');
+$config->load();
 
 $bot = new Bot();
 $bot->run();
