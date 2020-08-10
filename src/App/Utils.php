7ff6ff820976a96ef87d02894ded9ad3f8687e1c
LazyTechwork <lazytechwork@gmail.com>
2020-08-10 18:47:40 +0300

    Added logger

--- src/App/Utils.php
+++ src/App/Utils.php
@@ -6,6 +6,11 @@ namespace App;
 
 class Utils
 {
+    public static $LOG_INFO = "96m";
+    public static $LOG_WARN = "93m";
+    public static $LOG_ERROR = "91m";
+    public static $LOG_DEBUG = "37m";
+
     public static function identifySex($vk_sex)
     {
         switch ($vk_sex) {
@@ -16,5 +21,15 @@ class Utils
             default:
                 return "undefined";
         }
+    }
+
+    public static function log($msg, $lvl = "96m")
+    {
+        echo "\033[$lvl$msg\033[0m\n";
+    }
+
+    public static function debug($var)
+    {
+        self::log(print_r($var, true), self::$LOG_DEBUG);
     }
 }
